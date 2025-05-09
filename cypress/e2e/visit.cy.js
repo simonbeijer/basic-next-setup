d6284a4f504298789d6baf94cb534516e2d4ae8c
”simobeijer” <beijer.simon@gmail.com>
2025-05-09 10:28:11 +0200

    Update Cypress test

--- cypress/e2e/visit.cy.js
+++ cypress/e2e/visit.cy.js
@@ -1,13 +1,9 @@
 describe('Home Page', () => {
-  it('should visit the homepage and see the navbar', () => {
-    cy.visit('/');
-    cy.get('nav').should('exist');
-    cy.get('nav').should('contain', 'My navbar');
-  });
-
-  it('should click the Login button and navigate to login page', () => {
+  it('Visit home page and should click the Login button and navigate to login page', () => {
     cy.visit('/');
     cy.contains('Login').should('be.visible').click();
     cy.url().should('include', '/login');
+    cy.get('nav').should('exist');
+    cy.get('nav').should('contain', 'My navbar logged out');
   });
 });
