70562c169b2e6b22b8d464e9a6bf4684900ec2a1
”simobeijer” <beijer.simon@gmail.com>
2025-05-14 17:16:44 +0200

    Smoother dropdown css

--- src/app/components/dropdown.js
+++ src/app/components/dropdown.js
@@ -7,30 +7,42 @@ export default function Dropdown({ user, logoutUser }) {
   const [isOpen, setIsOpen] = useState(false);
   const toggleDropdown = () => setIsOpen((prev) => !prev);
 
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 7000);
+    }
+  }, [isOpen]);
+
   return (
     <div className="relative">
       <button className="flex items-center" onClick={toggleDropdown}>
         <UserCircleIcon className="h-8 w-8"></UserCircleIcon>
       </button>
-      {isOpen && (
-        <div className="absolute right-0 mt-2 p-4 border rounded-md shadow-md text-right">
-          {user ? (
-            <>
-              <p>{user.name}</p>
-              <button
-                className="text-[var(--grey)] font-bold"
-                onClick={logoutUser}
-              >
-                Logout
-              </button>
-            </>
-          ) : (
-            <div className="h-[82px] w-[88px]">
-              <Spinner />
-            </div>
-          )}
-        </div>
-      )}
+      <div
+        className={`transition-all duration-500 transform absolute right-0 mt-2 p-4 border rounded-md shadow-md text-right bg-black ${
+          isOpen
+            ? "opacity-100 scale-100"
+            : "opacity-0 scale-95 pointer-events-none"
+        }`}
+      >
+        {user ? (
+          <>
+            <p>{user.name}</p>
+            <button
+              className="text-[var(--grey)] font-bold"
+              onClick={logoutUser}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <div className="h-[60px] w-[60px] flex items-center justify-center">
+            <Spinner />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
