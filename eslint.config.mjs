84cd6635868136e00a29723e1e1eabbcceb9a931
”simobeijer” <beijer.simon@gmail.com>
2025-05-16 09:24:55 +0200

    Update lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,5 @@
-// eslint.config.mjs
-
-import { FlatCompat } from '@eslint/eslintrc';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import next from 'eslint-config-next';
 
 export default [
-  ...compat.extends('eslint:recommended', 'plugin:react/recommended', 'next'),
-  {
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
-      },
-    },
-    rules: {
-      // Add custom rules here
-    },
-  },
+  ...next(),
 ];
