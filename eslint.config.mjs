5b840f84f33a22a5d2a77aa49b37e528ace3f94d
”simobeijer” <beijer.simon@gmail.com>
2025-05-16 09:21:02 +0200

    Change eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,15 @@
-import { FlatCompat } from '@eslint/eslintrc';
+// eslint.config.mjs
 
-const compat = new FlatCompat();
+import { FlatCompat } from '@eslint/eslintrc';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default [
   ...compat.extends('eslint:recommended', 'plugin:react/recommended', 'next'),
@@ -13,7 +22,7 @@ export default [
       },
     },
     rules: {
-      // add or override rules here if needed
+      // Add custom rules here
     },
   },
 ];
