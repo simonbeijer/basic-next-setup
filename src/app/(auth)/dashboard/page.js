53baf51de9ae46a7b55c8ffa5310d447f50e0c02
”simobeijer” <beijer.simon@gmail.com>
2025-05-19 23:02:31 +0200

    Start re-deploy

--- src/app/(auth)/dashboard/page.js
+++ src/app/(auth)/dashboard/page.js
@@ -3,7 +3,7 @@
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen p-8">
-      <div className="flex flex-1 items-center justify-center bg-slate-400">
+      <div className="flex flex-1 items-center justify-center bg-violet-600">
         <h2>Dashboard</h2>
       </div>
     </div>
