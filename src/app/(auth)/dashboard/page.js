2437837f666b20b19477dbb6c2022971317e26c3
”simobeijer” <beijer.simon@gmail.com>
2025-05-26 17:46:51 +0200

    Move widget

--- src/app/(auth)/dashboard/page.js
+++ src/app/(auth)/dashboard/page.js
@@ -1,12 +1,10 @@
 "use client";
-import Widget from "@/app/components/widget";
 
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen p-8">
       <div className="flex flex-1 items-center justify-center bg-gray-100">
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-          <Widget text="WIDGET"/>
         </div>
       </div>
     </div>
--- src/app/components/widget.js
+++
@@ -1,7 +0,0 @@
-"use client"
-
-export default function Widget({text}) {
-    return (
-        <div class="bg-white shadow rounded p-4 text-black">{text}</div>
-    );
-};
