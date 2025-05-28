f0620738ed0b76c2e1459d98c28ce9153040ca7f
”simobeijer” <beijer.simon@gmail.com>
2025-05-28 21:25:01 +0200

    Add widget items to sidebar, for option

--- src/app/(auth)/dashboard/page.js
+++ src/app/(auth)/dashboard/page.js
@@ -7,9 +7,7 @@ export default function Dashboard() {
       <Sidebar />
       <div className="flex flex-1 items-center justify-center bg-gray-100">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-          <WeatherWidget>
-
-          </WeatherWidget>
+          <WeatherWidget />
         </div>
       </div>
     </div>
--- src/app/components/widget/sidebar.js
+++ src/app/components/widget/sidebar.js
@@ -1,7 +1,16 @@
 "use client";
 
 export default function Sidebar() {
-  return (<aside className="w-64 bg-white shadow p-4">
-
-  </aside>);
+  const widgetItems = ["Weater", "Stonks", "Cool shit"];
+  return(
+  <aside className="w-64 bg-white shadow p-4 text-[var(--background)]">
+  <ul>
+    {widgetItems.map((item, index) => (
+        <li key={index}>
+            {item}
+        </li>
+    ))}
+  </ul>
+  </aside>
+  );
 }
