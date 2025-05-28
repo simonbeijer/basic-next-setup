3e79e27cc824be22da39b2ab4a5b70a563648777
”simobeijer” <beijer.simon@gmail.com>
2025-05-28 19:44:19 +0200

    Add sidebar, selecting displayed widgets

--- src/app/(auth)/dashboard/page.js
+++ src/app/(auth)/dashboard/page.js
@@ -1,8 +1,10 @@
 "use client";
 import WeatherWidget from "../../components/widget/weatherWidget";
+import Sidebar from "@/app/components/widget/sidebar";
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen p-8">
+      <Sidebar />
       <div className="flex flex-1 items-center justify-center bg-gray-100">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
           <WeatherWidget>
---
+++ src/app/components/widget/sidebar.js
@@ -0,0 +1,7 @@
+"use client";
+
+export default function Sidebar() {
+  return (<aside className="w-64 bg-white shadow p-4">
+
+  </aside>);
+}
